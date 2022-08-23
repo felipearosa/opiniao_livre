a3b6dbfcbf30b891af1520fc752827e670db86b9
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <lucas.mturra@gmail.com>
2022-08-23 11:36:54 -0300

    add new create

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,14 @@
 class OffersController < ApplicationController
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
