572738bbd8bb9abd546682c2cae5f0bbf44d9649
Eduardo Thomazoni <eduardotps@gmail.com>
2022-08-23 11:42:56 -0300

    update controller, form, and view created

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,21 @@
 class OffersController < ApplicationController
+  def edit
+    @offer = Offer.find(params[:id])
+  end
+
+  def update
+    @offer = Offer.find(params[:id])
+    @offer.update(offer_params)
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :price, :media, :niche, :reach)
+  end
 end
