c1ce3e1eb7ec5225f77c4d7c8956cff1c596655d
Felipe Arosa <felipe.arosa@gmail.com>
2022-08-24 16:07:50 -0300

    feito

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,15 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
 
-User.destroy_all
-Offer.destroy_all
+arr = ['Corrente de Whatsapp', 'Vlog no Youtube', 'Artigo Jurídico', 'Stories no Instagram', 'Dancinha no TikTok', 'Tweet', 'Coluna no Jornal']
+
 puts 'Creating...'
-user = User.new(first_name: Faker::Games::SuperMario.character, last_name: Faker::Games::SuperMario.location, email: "test@test.com")
+user = User.new(first_name: Faker::Games::SuperMario.character, last_name: Faker::Games::SuperMario.location, email: "test2@test.com")
 user.password = 'valid_password'
 user.password_confirmation = 'valid_password'
 user.save!
 10.times do
-  offer = Offer.new(title: Faker::TvShows::TwinPeaks.quote, description: Faker::Hacker.say_something_smart, price: rand(1...500), media: Faker::Cannabis.brand, niche: Faker::Cannabis.cannabinoid, reach: rand(1000...50000))
+  offer = Offer.new(title: Faker::TvShows::TwinPeaks.quote, description: Faker::Hacker.say_something_smart, price: rand(1...500), media: arr.sample, niche: Faker::Cannabis.cannabinoid, reach: rand(1000...50000))
   offer.user = User.last
   offer.save!
 end
