8c53c91a5d3389ebfd22c26f6f692fa039f3c6a8
Felipe Arosa <felipe.arosa@gmail.com>
2022-08-24 15:26:11 -0300

    changed

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,7 @@
 class Offer < ApplicationRecord
+  CATEGORIES = ['Corrente de Whatsapp', 'Vlog no Youtube', 'Artigo Jurídico', 'Stories no Instagram', 'Dancinha no TikTok', 'Tweet', 'Coluna no Jornal']
+
+
   belongs_to :user
 
   has_many :deals
@@ -6,7 +9,12 @@ class Offer < ApplicationRecord
   has_many :users, through: :reviews
   validates :title, :description, presence: true
 
+
   def price_installments
     price.to_f / 10
   end
+
+  # def image
+  #   if category
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  CATEGORIES = ['Tia do Zap', 'Youtuber', 'Advogado', 'Influencer de Instagram', 'TikToker', 'Twitteiro', 'Jornalista']
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :offers
   has_many :deals
   has_many :reviews
-  
+
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  # def image
+  #   if category
+  # end
 end
