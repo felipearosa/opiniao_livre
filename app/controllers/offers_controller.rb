5825513c7db56c495b57e7348f7f30968772dbee
Felipe Arosa <felipe.arosa@gmail.com>
2022-08-25 18:11:43 -0300

    adjusted error

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -25,7 +25,7 @@ class OffersController < ApplicationController
   def create
     @offer = Offer.new(offer_params)
     @offer.user = current_user
-    authotize @offer
+    authorize @offer
     if @offer.save
       redirect_to offer_path(@offer)
     else
